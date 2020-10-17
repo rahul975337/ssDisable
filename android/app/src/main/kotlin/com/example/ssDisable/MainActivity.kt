8d1c79822c70590b113c3e45f9919f47fb37d601
rahul975337 <tilwani03@gmail.com>
2020-10-18 01:53:39 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/ssDisable/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ssDisable
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/hey.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_windowmanager/flutter_windowmanager.dart';
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  /////////
+  Future<void> secureScreen() async {
+    await FlutterWindowManager.addFlags(FlutterWindowManager.FLAG_SECURE);
+    await FlutterWindowManager.addFlags(Flutter)
+  }
+
+  @override
+  void initState() {
+    secureScreen();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'This page is secured',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
